8a4a08a6e7d10c376ae2559cfade4148442d61f3
Paul Zietsman <paulzietsman@Pauls-MacBook-Pro.local>
2019-01-09 21:02:14 +0200

    env and aws creds

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+region = ${AWS_REGION}
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOLa
