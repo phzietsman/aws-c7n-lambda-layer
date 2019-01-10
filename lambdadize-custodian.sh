c215aea01ac8c689ed0f249125c34593814091b2
Paul Zietsman <paulzietsman@Pauls-MacBook-Pro.local>
2019-01-10 09:07:22 +0200

    custodian layer shift v2

--- lambdadize-custodian.sh
+++ lambdadize-custodian.sh
@@ -5,4 +5,5 @@ mkdir -p /tmp/custodian-layer
 cp -r /tmp/custodian/lib/python2.7/site-packages/. /tmp/custodian-layer/
 cp /tmp/custodian/bin/custodian /tmp/custodian-layer/.
 sed -i '1s/^.*$/# No shbang/' /tmp/custodian-layer/custodian
-zip -r content.zip /tmp/custodian-layer/
+cd /tmp/
+zip -r /tmp/content.zip custodian-layer/
