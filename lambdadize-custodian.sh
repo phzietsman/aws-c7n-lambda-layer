a381ed5a4168427537d4a8993dbec1fb6747cf05
Paul Zietsman <paulzietsman@Pauls-MacBook-Pro.local>
2019-01-10 08:53:05 +0200

    Lambda layer shift

--- lambdadize-custodian.sh
+++ lambdadize-custodian.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/custodian
+mkdir -p /tmp/custodian-layer
 
-cp custodian/lib/python2.7/site-packages/. /tmp/custodian/
-cp custodian/bin/custodian /tmp/custodian/.
-sed -i '1s/^.*$/# No shbang/' /tmp/custodian/custodian
-zip -r content.zip /tmp/custodian/
+cp -r /tmp/custodian/lib/python2.7/site-packages/. /tmp/custodian-layer/
+cp /tmp/custodian/bin/custodian /tmp/custodian-layer/.
+sed -i '1s/^.*$/# No shbang/' /tmp/custodian-layer/custodian
+zip -r content.zip /tmp/custodian-layer/
